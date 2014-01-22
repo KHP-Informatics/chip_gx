c05e06996ef56b05a93f15da343a779d745c105e
Dr Stephen Newhouse <stephen.j.newhouse@gmail.com>
2014-01-22 11:56:45 +0000

    almost done

--- Illumina_expression_workflow/create_pdf.sh
+++ Illumina_expression_workflow/create_pdf.sh
@@ -2,6 +2,10 @@
 
 RMDFILE=GAP_gx_illumina_pipeline_preProcessing_step_001;
 
-Rscript -e "require(knitr); require(markdown); knit('$RMDFILE.Rmd', '$RMDFILE.md');markdownToHTML('$RMDFILE.md', '$RMDFILE.html', options=c('use_xhml'));"
+Rscript -e "require(knitr); require(markdown); knit('$RMDFILE.Rmd','$RMDFILE.md','$RMDFILE.tex'); markdownToHTML('$RMDFILE.md', '$RMDFILE.html', options=c('use_xhml'));"
 
 pandoc -s GAP_gx_illumina_pipeline_preProcessing_step_001.html -o GAP_gx_illumina_pipeline_preProcessing_step_001.pdf
+
+
+
+knit2html('$RMDFILE.Rmd')
